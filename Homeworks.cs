88b91ac1ec3f691434e561f3bcaba4e672786f69
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-08-26 20:03:21 +0300

    почти закончил

--- Homeworks.cs
+++ Homeworks.cs
@@ -3,7 +3,17 @@
 
  Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
 является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
 */
 
+double a = 0;
+
 Console.WriteLine("Введите число для проверки на четность");
-double a = double.TryParse
+double.TryParse(Console.ReadLine(), out a);
+
+if (a % 2 == 0)
+    Console.WriteLine($"{a} да, четное ");
+
+Console.WriteLine($"{a} нет, нечетное ");
