d95521aa91e28ca25561031ce3d6df2883b0e31f
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-08-26 18:09:47 +0300

    Task solved for pos num

--- Homeworks.cs
+++ Homeworks.cs
@@ -6,4 +6,17 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
 */
 
-Console.WriteLine("Hello, World!");
+double a = 0, b = 0, max = 0;
+
+Console.WriteLine("Введите первое число");
+double.TryParse(Console.ReadLine(), out a);
+
+Console.WriteLine("Введите второе число");
+double.TryParse(Console.ReadLine(), out b);
+
+if (a < b)
+    max = a;
+max = b;
+
+Console.Write($"max = {max}");
+// а как сделать сравнение для отрицательных чисел? Нужна логика.
