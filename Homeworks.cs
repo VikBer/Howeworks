c7542f18e94817c8e016261ba83f3cc0b617a75b
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-08-28 19:22:09 +0300

    Triangle Serpinskogo

--- Homeworks.cs
+++ Homeworks.cs
@@ -1,16 +1,46 @@
 ﻿// Для перехода на другие ветки вернитесь в оглавнение -  git checkout master
+Console.Clear();
+
 int xa = 40;
-int ya = 45;
-int xb = 32;
-int yb = 38;
-int xc = 35;
-int yc = 40;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 80;
+int yc = 30;
 
-Console.SetCursorPosition(xa,ya);
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-Console.SetCursorPosition(xb,yb);
-Console.WriteLine("+");
+int x = xa, y = ya;
 
-Console.SetCursorPosition(xc,yc);
-Console.WriteLine("+");
+int count = 10; 
+
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0) 
+    { 
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+    }
+    if (what ==1)
+    {
+        x = (x+xb)/2;
+        y = (y+yb)/2;
+    }
+    if (what ==2)
+    {
+        x = (x+xc)/2;
+        y = (y+yc)/2;
+    }
+    Console.SetCursorPosition(x,y);
+    Console.WriteLine("+");
+    count++;
+
+
+}
+
