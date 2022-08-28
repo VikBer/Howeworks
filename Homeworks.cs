ebdadfbf69e5b59d791ba4f5161a26c7fdfec45f
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-08-28 18:30:42 +0300

    i не работает чето

--- Homeworks.cs
+++ Homeworks.cs
@@ -1,20 +1,16 @@
 ﻿// Для перехода на другие ветки вернитесь в оглавнение -  git checkout master
-/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int xa = 40;
+int ya = 45;
+int xb = 32;
+int yb = 38;
+int xc = 35;
+int yc = 40;
 
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3
-*/
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine("+");
 
-double a = 0, b = 0, max = 0;
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine("+");
 
-Console.WriteLine("Введите первое число");
-double.TryParse(Console.ReadLine(), out a);
-
-Console.WriteLine("Введите второе число");
-double.TryParse(Console.ReadLine(), out b);
-
-if (a > b)    max = a;
-else max = b;
-
-Console.Write($"max = {max}");
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine("+");
