f23a1b9f6895ad11789befba636c6c24e71e81fc
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-09-08 19:11:03 +0300

    task solved

--- Homeworks.cs
+++ Homeworks.cs
@@ -5,3 +5,23 @@
 [-4, -6, 89, 6] -> 0
 
 */
+
+int[] array = new int [new Random().Next(4,9)];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100,100);
+}
+
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0 ) {sum+= array[i];}
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.Write($"--> {sum} ");
